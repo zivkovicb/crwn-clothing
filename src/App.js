368f8a21e32b1a9bb14c7e758fd9d869ce3b4ad4
BORIS\Boris <b.zivkovic97@yahoo.com>
2021-11-20 14:57:52 +0100

    started the shop page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
