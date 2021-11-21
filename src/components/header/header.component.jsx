9ca2895a25cf3290466278e5425181268c97c2d9
BORIS\Boris <b.zivkovic97@yahoo.com>
2021-11-21 12:02:15 +0100

    fix the position of shop and contact text

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,19 +6,19 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 
 const Header = () => (
-    <div className='header'>
-        <Link className='logo-container' to="/">
-            <Logo className='logo' />
-        </Link>
-        <div className='opitons'>
-            <Link className='option' to='/shop'>
-                SHOP 
-            </Link>
-            <Link className='option' to='/contact'>
-                CONTACT
-            </Link>
-        </div>
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
+    </Link>
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        SHOP
+      </Link>
+      <Link className='option' to='/shop'>
+        CONTACT
+      </Link>
     </div>
+  </div>
 );
 
 export default Header;
