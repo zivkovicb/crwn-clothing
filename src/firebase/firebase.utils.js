2b743e33472071058882f8333578ce8e27c94276
BORIS\Boris <b.zivkovic97@yahoo.com>
2022-03-07 10:15:58 +0100

    tranfered shop datat to firebase, and connected it with redux

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -73,7 +73,25 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
   });
 
   return await batch.commit();
-} 
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    }
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {}) 
+}
 
 const provider = new GoogleAuthProvider();
 provider.setCustomParameters({
